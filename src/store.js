40893b99be764ba316085c4278cc17d85fe4d1fc
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-27 12:54:55 +0100

    Added a new reducer for customer features

--- src/store.js
+++ src/store.js
@@ -1,11 +1,31 @@
 import { createStore } from "redux"
+
 const inititalState = {
     balance: 0,
     loan: 0,
-    loanPurpose: ''
+    loanPurpose: '',
+    fullName: '',
+    nationalID: ''
 };
 
-function reducer(state = inititalState, action) {
+const inititalStateCustomer = {
+    fullName: '',
+    nationalID: '',
+    createdAt: ''
+};
+
+function customerReducer(state = inititalStateCustomer, action) {
+    switch (action.type) {
+        case "customer/createCustomer":
+            return { ...state, fullName: action.payload.fullName, nationalID: action.payload.nationalID, createdAt: action.payload.createdAt }
+        case "customer/updateName":
+            return { ...state, fullName: action.payload }
+        default:
+            return state;
+    }
+}
+
+function accountReducer(state = inititalState, action) {
     switch (action.type) {
         case "account/deposit":
             return { ...state, balance: state.balance + action.payload }
@@ -14,7 +34,7 @@ function reducer(state = inititalState, action) {
         case "account/requestLoan":
             if (state.loan > 0)
                 return state
-            return { ...state, loan: action.payload.amount, loanPurpose: action.payload.purpose }
+            return { ...state, loan: action.payload.amount, loanPurpose: action.payload.purpose, balance: state.balance + action.payload.amount }
         case "account/payLoan":
             return { ...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan }
         default:
@@ -22,7 +42,7 @@ function reducer(state = inititalState, action) {
     }
 }
 
-const store = createStore(reducer);
+const store = createStore(accountReducer, customerReducer);
 
 function deposit(amount) {
     return { type: 'account/deposit', payload: amount };
@@ -42,3 +62,14 @@ function requestLoan(amount, purpose) {
 function payLoan(amount) {
     return { type: 'account/payLoan' };
 };
+
+function updateName(fullName) {
+    return { type: 'account/updateName', payload: fullName }
+}
+
+function createCustomer(fullName, nationalID) {
+    return {
+        type: 'customer/createCustomer',
+        payload: { fullName, nationalID, createdAt: new Date().toISOString() }
+    }
+}
