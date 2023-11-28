451d3db86e297c343cffaf2cac07dd3b81bcef6a
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-28 18:36:53 +0100

    Fixed imports

--- src/features/customers/CreateCustomer.js
+++ src/features/customers/CreateCustomer.js
@@ -1,31 +1,38 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createCustomer } from "./customerSlice";
 
 function Customer() {
   const [fullName, setFullName] = useState("");
   const [nationalId, setNationalId] = useState("");
-
-  function handleClick() {}
+  const dispatch = useDispatch();
+  function handleClick() {
+    if (!fullName || !nationalId) {
+      return;
+    }
+    dispatch(createCustomer(fullName, nationalId));
+  }
 
   return (
     <div>
-      <h2>Create new customer</h2>
+      <h2> Create new customer </h2>{" "}
       <div className="inputs">
         <div>
-          <label>Customer full name</label>
+          <label> Customer full name </label>{" "}
           <input
             value={fullName}
             onChange={(e) => setFullName(e.target.value)}
-          />
-        </div>
+          />{" "}
+        </div>{" "}
         <div>
-          <label>National ID</label>
+          <label> National ID </label>{" "}
           <input
             value={nationalId}
             onChange={(e) => setNationalId(e.target.value)}
-          />
-        </div>
-        <button onClick={handleClick}>Create new customer</button>
-      </div>
+          />{" "}
+        </div>{" "}
+        <button onClick={handleClick}> Create new customer </button>{" "}
+      </div>{" "}
     </div>
   );
 }
