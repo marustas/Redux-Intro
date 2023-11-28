d65664db613b17875b713464c7a4fdad51c7a9fc
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-28 18:59:29 +0100

    Added account balance manipulations

--- src/features/account/AccountOperations.js
+++ src/features/account/AccountOperations.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deposit, requestLoan, withdraw, payLoan } from "./accountSlice";
 
 function AccountOperations() {
   const [depositAmount, setDepositAmount] = useState("");
@@ -7,48 +9,83 @@ function AccountOperations() {
   const [loanPurpose, setLoanPurpose] = useState("");
   const [currency, setCurrency] = useState("USD");
 
-  function handleDeposit() {}
+  const dispatch = useDispatch();
+  const {
+    loan: currentLoan,
+    loanPurpose: currentLoanPurpose,
+    balance,
+  } = useSelector((store) => store.account);
 
-  function handleWithdrawal() {}
+  function handleDeposit() {
+    if (!depositAmount) {
+      return;
+    } else {
+      dispatch(deposit(depositAmount));
+      setDepositAmount("");
+    }
+  }
 
-  function handleRequestLoan() {}
+  function handleWithdrawal() {
+    if (!withdrawalAmount) {
+      return;
+    } else {
+      dispatch(withdraw(withdrawalAmount));
+      setWithdrawalAmount("");
+    }
+  }
 
-  function handlePayLoan() {}
+  function handleRequestLoan() {
+    if (!loanAmount || loanPurpose) {
+      return;
+    } else {
+      dispatch(requestLoan(loanAmount, loanPurpose));
+      setLoanAmount("");
+      setLoanPurpose("");
+    }
+  }
+
+  function handlePayLoan() {
+    if (!loanAmount) {
+      return;
+    } else {
+      dispatch(payLoan());
+    }
+  }
 
   return (
     <div>
-      <h2> Your account operations </h2>{" "}
+      <h2> Your account operations </h2>
       <div className="inputs">
         <div>
-          <label> Deposit </label>{" "}
+          <label> Deposit </label>
           <input
             type="number"
             value={depositAmount}
             onChange={(e) => setDepositAmount(+e.target.value)}
-          />{" "}
+          />
           <select
             value={currency}
             onChange={(e) => setCurrency(e.target.value)}
           >
-            <option value="USD"> US Dollar </option>{" "}
-            <option value="EUR"> Euro </option>{" "}
-            <option value="GBP"> British Pound </option>{" "}
-          </select>
-          <button onClick={handleDeposit}> Deposit {depositAmount} </button>{" "}
-        </div>
+            <option value="USD"> US Dollar </option>
+            <option value="EUR"> Euro </option>
+            <option value="GBP"> British Pound </option>
+          </select>{" "}
+          <button onClick={handleDeposit}> Deposit {depositAmount} </button>
+        </div>{" "}
         <div>
-          <label> Withdraw </label>{" "}
+          <label> Withdraw </label>
           <input
             type="number"
             value={withdrawalAmount}
             onChange={(e) => setWithdrawalAmount(+e.target.value)}
-          />{" "}
+          />
           <button onClick={handleWithdrawal}>
-            Withdraw {withdrawalAmount}{" "}
-          </button>{" "}
+            Withdraw {withdrawalAmount}
+          </button>
         </div>
         <div>
-          <label> Request loan </label>{" "}
+          <label> Request loan </label>
           <input
             type="number"
             value={loanAmount}
@@ -60,13 +97,17 @@ function AccountOperations() {
             onChange={(e) => setLoanPurpose(e.target.value)}
             placeholder="Loan purpose"
           />
-          <button onClick={handleRequestLoan}> Request loan </button>{" "}
+          <button onClick={handleRequestLoan}> Request loan </button>
         </div>
-        <div>
-          <span> Pay back $X </span>{" "}
-          <button onClick={handlePayLoan}> Pay loan </button>{" "}
-        </div>{" "}
-      </div>{" "}
+        {currentLoan > 0 && (
+          <div>
+            <span>
+              Pay back $ {currentLoan}({currentLoanPurpose})
+            </span>
+            <button onClick={handlePayLoan}> Pay loan </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/features/customers/CreateCustomer.js
+++ src/features/customers/CreateCustomer.js
@@ -15,24 +15,24 @@ function Customer() {
 
   return (
     <div>
-      <h2> Create new customer </h2>{" "}
+      <h2> Create new customer </h2>
       <div className="inputs">
         <div>
-          <label> Customer full name </label>{" "}
+          <label> Customer full name </label>
           <input
             value={fullName}
             onChange={(e) => setFullName(e.target.value)}
-          />{" "}
-        </div>{" "}
+          />
+        </div>
         <div>
-          <label> National ID </label>{" "}
+          <label> National ID </label>
           <input
             value={nationalId}
             onChange={(e) => setNationalId(e.target.value)}
-          />{" "}
-        </div>{" "}
+          />
+        </div>
         <button onClick={handleClick}> Create new customer </button>{" "}
-      </div>{" "}
+      </div>
     </div>
   );
 }
